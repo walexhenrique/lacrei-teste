bea838a4a57bb13a0dd01b031140ff5948fbdf84
walexhenrique <walex999067@gmail.com>
2022-10-16 00:48:22 -0300

    Criado model de tarefa e adicionado no admin

--- tarefas/admin.py
+++ tarefas/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Tarefa
+
+
+@admin.register(Tarefa)
+class TarefaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'titulo', 'data_e_hora_de_criacao', 'ultima_modificacao', 'finalizado',)
+    list_display_links = ('titulo',)
+    list_editable = ('finalizado',)
+    list_filter = ('id', 'titulo', 'finalizado',)
--- tarefas/models.py
+++ tarefas/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Tarefa(models.Model):
+    titulo = models.CharField(max_length=50)
+    descricao = models.CharField(max_length=150)
+    data_e_hora_de_criacao = models.DateTimeField(auto_now_add=True)
+    ultima_modificacao = models.DateTimeField(auto_now=True)
+    finalizado = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return self.titulo
+    
